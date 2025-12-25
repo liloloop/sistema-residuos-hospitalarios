892c7b54cf611324fbb0ac40811419951a7b5a2c
liloop <glorialilo@gmail.com>
2025-12-25 12:13:49 -0500

    Clean up unused imports in dashboard_residuos.py
    Removed unused imports for numpy, seaborn, and matplotlib.

--- dashboard_residuos.py
+++ dashboard_residuos.py
@@ -8,12 +8,9 @@ Versión: 1.0 PILOTO | Propuesta: Clasificación QR Semiautomatizada
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
-import seaborn as sns
-import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import warnings
 warnings.filterwarnings('ignore')
